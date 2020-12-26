eed437035bcf687e27ccc2f4aa4ceb638552ead3
exoticchild <therealenny1@gmail.com>
2020-12-26 15:45:41 -0600

    exec
    exec

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function eventfn(event){
 "What do you get when you cross a vampire and a snowman?<br>Frost bite!", "What did one plate say to the other plate?<br>Dinner is on me!", 
 "Why did the student eat his homework?<br>Because the teacher told him it was a piece of cake!", "When you look for something, why is it always in the last place you look?<br>Because when you find it, you stop looking.", 
 "What is brown, hairy and wears sunglasses?<br>A coconut on vacation.", "Two pickles fell out of a jar onto the floor. What did one say to the other?<br>Dill with it.", 
-"What did the Dalmatian say after lunch?<br>That hit the spot!"]
+"What did the Dalmatian say after lunch?"]
   let res = ""
   let res1 = ""
   switch (command) {
@@ -34,6 +34,36 @@ function eventfn(event){
     //urls
     case (prefix + "youtube.url"):
       res = "<a href='https://www.youtube.com'>Youtube</a>";
+      break;
+    //music
+    //avicii
+    case (prefix + "music.friend.of.mine-avicii"):
+      res = "<iframe src='./music/avicii-friendofmine.mp3'>";
+      break;
+    case (prefix + "music.lonely.together-avicii"):
+      res = "<iframe src='./music/avicii-lonelytogether.mp3'>";
+      break;
+    case (prefix + "music.waiting.for.love-avicii"):
+      res = "<iframe src='./music/avicii-waitingforlove.mp3'>";
+      break;
+    case (prefix + "music.sos-avicii"):
+      res = "<iframe src='./music/avicii-sos.mp3'>";
+      break;
+    case (prefix + "music.what.would.i.change.it.to-avicii"):
+      res = "<iframe src='./music/avicii-whatwouldichangeitto.mp3'>";
+      break;
+    case (prefix + "music.without.you-avicii"):
+      res = "<iframe src='./music/avicii-withoutyou.mp3'>";
+      break;
+    case (prefix + "music.you.be.love-avicii"):
+      res = "<iframe src='./music/avicii-youbelove.mp3'>";
+      break;
+    //lil xxel
+    case (prefix + "music.idk-lil.xxel"):
+      res = "<iframe src='./music/lilxxel-idk.mp3'>";
+      break;
+    case (prefix + "music.lmk-lil.xxel"):
+      res = "<iframe src='./music/lilxxel-lmk.mp3'>";
       break;
     //location
     case (prefix + "current.url"):
